9181f3f1ad7b5f6794e52de7899136eb339f70cc
delmar2022 <delmaramon@gmail.com>
2024-01-21 11:34:49 -0600

    Inicio de Clase Univo

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delmarts</title>
+</head>
+<body>
+
+<form action="" method="POST" role="form">
+    <legend>Info Personal</legend>
+
+    <div class="form-group">
+        <label for="">Nombres</label>
+        <input type="text" class="form-control" id="" placeholder="Input field">
+    </div>
+    <div class="form-group">
+        <label for="">Apellidos</label>
+        <input type="text" class="form-control" id="" placeholder="Input field">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</body>
+</html>
