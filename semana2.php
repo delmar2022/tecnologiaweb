ab6fea8dccf618e5edced3ecad4730464a516672
delmar2022 <delmaramon@gmail.com>
2024-01-28 09:33:06 -0600

    Estructura de PHP

---
+++ semana2.php
@@ -0,0 +1,25 @@
+<?php
+//Definir las Variables
+$nombre = "juan";
+$edad = 25;
+
+//Funiones simples
+function saludar ($nombre, $edad){
+    echo "Hola, $nombre. Tienes $edad anios.<br>";
+}
+//llamada a la funcion
+saludar($nombre, $edad);
+
+//Estructuras condicionales
+if ($edad>=18) {
+    echo 'Eres MAYOR de edad';
+}else{
+    echo 'Eres MENOR de edad';
+}
+
+//Bucle for 
+
+// Array
+
+//Blucle foreach
+?>
